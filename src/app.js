d0f5643e5f107d189e076da6382f7fb8950f8d4d
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-08-27 13:38:20 +0200

    make function async in callbabck get/post methods

--- src/app.js
+++ src/app.js
@@ -8,41 +8,42 @@ const User = require('./db/models/user')
 
 app.use(express.json())
 
-app.post('/user', (req, res) => {
-    console.log(req.body)
+app.post('/user', async(req, res) => {
     if (!req.body) {
         return res.send('No user sent')
     }
 
-    const { name, email, password } = req.body
-    const user = new User(req.body)
-    user.save().then(() => {
-        res.send(user)
-    }).catch((err) => {
+    try {
+        const user = new User(req.body)
+        await user.save()
+        res.status(201).send(user)
+
+    } catch (err) {
         res.status(400).send(err)
-    })
+    }
 
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async(req, res) => {
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((err) => {
+    } catch (err) {
         res.end(err)
-    })
+    }
 })
 
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id
-    User.findById(_id).then((user) => {
+app.get('/users/:id', async(req, res) => {
+    try {
+        const _id = req.params.id
+        const user = await User.findById(_id)
         if (!user) {
             return res.status(404).send()
         }
-
         res.send(user)
-    }).catch((err) => {
+    } catch (err) {
         res.status(500).send(err)
-    })
+    }
 })
 
 app.listen(port)
